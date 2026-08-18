42356381e4296a88e3b40d6509ffd6b7088a27c3
khlongwa-dev <ayandahlongwa21@gmail.com>
2026-08-18 09:46:46 +0200

    script: wrote and added a script for adding one user with a forced password change.

---
+++ tools/users_and_groups/create_one_user.ps1
@@ -0,0 +1,12 @@
+# How to create one user with default password and a forced password change on first logon
+
+$Username = "khlongwa"
+$DefaultPassword = ConvertTo-SecureString "Voltcore@2026!" -AsPlainText -forced
+
+# Create the user instantly
+New-LocalUser -Name $Username -Password $DefaultPassword -FullName "Kusasalakhe Hlongwa" -Description "This is just a general user."
+
+# Force password change on next logon
+$User = [ADSI]"WinNT://localhost/$Username"
+$User.passwordExpired = 1
+$User.SetInfo()
