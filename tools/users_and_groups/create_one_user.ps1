d835cfe4cd9106ec7bd561d7f1697ed7816f51ec
khlongwa-dev <ayandahlongwa21@gmail.com>
2026-08-19 11:44:28 +0200

    fix: fixed the typo on create_one_user script, -forced -> -Force

--- tools/users_and_groups/create_one_user.ps1
+++ tools/users_and_groups/create_one_user.ps1
@@ -1,7 +1,7 @@
 # How to create one user with default password and a forced password change on first logon
 
 $Username = "khlongwa"
-$DefaultPassword = ConvertTo-SecureString "Voltcore@2026!" -AsPlainText -forced
+$DefaultPassword = ConvertTo-SecureString "Voltcore@2026!" -AsPlainText -Force
 
 # Create the user instantly
 New-LocalUser -Name $Username -Password $DefaultPassword -FullName "Kusasalakhe Hlongwa" -Description "This is just a general user."
