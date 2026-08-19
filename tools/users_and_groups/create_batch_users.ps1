3784d81170a25af08f90bc544259fbedda6c77aa
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2026-08-19 14:14:06 +0200

    fix:  fixed the passing of a password variable to be within a scope of a function.

--- tools/users_and_groups/create_batch_users.ps1
+++ tools/users_and_groups/create_batch_users.ps1
@@ -1,37 +1,44 @@
-$Password = ConvertTo-SecureString "Voltcore@2026!" -AsPlainText -Force
-
 function Create-Users {
+    $Password = ConvertTo-SecureString "Voltcore@2026!" -AsPlainText -Force
+    
     [CmdletBinding(SupportsShouldProcess)]
     param(
         [Parameter(Mandatory)]
         [string]$FilePath
     )
-
-    $results = foreach ($row in Import-Csv $FilePath) {
-
-        try {
-            New-LocalUser -Name $row.Username -Password $Password -FullName $row.FullName -Description "Voltcore employee."
-
-            $User = [ADSI]"WinNT://localhost/$Username"
-            $User.passwordExpired = 1
-            $User.SetInfo()
-
-            [PSCustomObject]@{
-                Name = $row.FullName
-                Status = "Success"
-                Message = "Created successfully"
+    
+    Write-Verbose "Creating Voltcore Employees..."
+    
+    if ($PSCmdlet.ShouldProcess($FilePath, "Create a batch of local users from $FilePath") {
+        $results = foreach ($row in Import-Csv $FilePath) {
+    
+            try {
+                New-LocalUser -Name $row.Username -Password $Password -FullName $row.FullName -Description "Voltcore employee." -ErrorAction Stop
+    
+                $User = [ADSI]"WinNT://localhost/$($row.Username)"
+                $User.passwordExpired = 1
+                $User.SetInfo()
+    
+                [PSCustomObject]@{
+                    Name = $row.FullName
+                    Status = "Success"
+                    Message = "Created successfully"
+                }
             }
-        }
-        catch {
-            [PSCustomObject]@{
-                Name = $row.Username
-                Status = "Failed"
-                Message = $_.Exception.Message
+            catch {
+                [PSCustomObject]@{
+                    Name = $row.Username
+                    Status = "Failed"
+                    Message = $_.Exception.Message
+                }
             }
         }
     }
 
+    Write-Verbose "Creating users complete."
     $results | Export-Csv .\user-creation-log.csv -NoTypeInformation
-
-    # This script is still under works, it has not been tested.
 }
+
+# Example invocation:
+# Create-Users -FilePath ".\users.csv" -Verbose
+# Create-Users -FilePath ".\users.csv" -WhatIf
